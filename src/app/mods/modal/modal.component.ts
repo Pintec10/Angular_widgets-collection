2ac1c64f5aa48d1b51b5f828034609b2d4daee20
Roberto Milani <milani@strands.com>
2021-10-29 14:28:52 +0200

    added ModsHome and Modal components. Implemented ngOnDestroy() for Modal, missing open/close still

---
+++ src/app/mods/modal/modal.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+
+  constructor(private el: ElementRef) { }
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    this.el.nativeElement.remove();
+  }
+
+}
